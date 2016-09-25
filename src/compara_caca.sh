a54c37d393413b24fb0fb542f3688cdee9f10a2c
Darkly Dreaming Wesker <weskerhluffy@gmail.com>
2016-09-25 00:21:13 -0500

    se imprimen tiempos de excepción

--- src/compara_caca.sh
+++ src/compara_caca.sh
@@ -3,6 +3,7 @@
 ulimit -c unlimited
 ulimit -c
 rm -fv /cores/*
+rm -fv core*
 for caca in $(ls *txt)
 do
 	caca_truncada=""
@@ -15,14 +16,18 @@ do
 	caca_log_piton=$caca_truncada.piton.log
 	echo "la caca en c $caca_log_c"
 	echo "la caca en piton $caca_log_piton"
+	date
 	./caca_x < $caca > $caca_log_c
+	date
 	return_codi=$?
 	if [ $return_codi -ne 0 ] 
 	then
 		echo "verga, fallo en $caca"
 		exit 1
 	fi
+	date
 	python cagada/shit.py < $caca > $caca_log_piton
+	date
 	diferencia_ojala=$(diff $caca_log_c $caca_log_piton)
 	echo "la diferencia es"
 	echo "$diferencia_ojala CACA"
